e4e2ccc33b547d77e6974613cdbce7d30aac91be
rosalynblanco <rblanco@adfone.com>
2021-03-17 22:34:15 -0400

    New features IV:
    - Test implementation

--- app/src/test/java/com/rosalynbm/locationreminder/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/rosalynbm/locationreminder/locationreminders/data/FakeDataSource.kt
@@ -2,26 +2,44 @@ package com.rosalynbm.locationreminder.locationreminders.data
 
 import com.rosalynbm.locationreminder.locationreminders.data.dto.ReminderDTO
 import com.rosalynbm.locationreminder.locationreminders.data.dto.Result
+import java.util.ArrayList
 
-//Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
+/**
+ * FakeDataSource acts as a test double to the LocalDataSource. Replaces the Data Layer
+ * to test the app in isolation.
+ */
+//ros class FakeDataSource(var reminders: MutableList<ReminderDTO>? = mutableListOf()) : ReminderDataSource {
+open class FakeDataSource : ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
+    private val reminder1 = ReminderDTO("reminder 1", "description 1",
+        "American Airlines Arena", 25.781374, -80.187917, "1")
+    private val reminder2 = ReminderDTO("reminder 2", "description 2",
+        "American Airlines Arena", 25.781374, -80.187917, "2")
+    private val reminder3 = ReminderDTO("reminder 3", "description 3",
+        "American Airlines Arena", 25.781374, -80.187917, "3")
+    private val reminders: MutableList<ReminderDTO> = mutableListOf(reminder1, reminder2, reminder3)
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        reminders.let { return Result.Success(ArrayList(it)) }
+
+        //return Result.Error("Reminders not found", 404)
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        reminders.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        val reminder = reminders.find { it.id == id }
+
+        return if (reminder != null)
+            Result.Success(reminder)
+        else
+            Result.Error("Reminder not found", 404)
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        reminders.clear()
     }
 
 
--- app/src/test/java/com/rosalynbm/locationreminder/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/rosalynbm/locationreminder/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,16 +1,88 @@
 package com.rosalynbm.locationreminder.locationreminders.savereminder
 
+import android.os.Build
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.rosalynbm.locationreminder.locationreminders.data.FakeDataSource
+import com.rosalynbm.locationreminder.locationreminders.data.dto.ReminderDTO
+import com.rosalynbm.locationreminder.locationreminders.data.dto.Result
+import com.rosalynbm.locationreminder.locationreminders.reminderslist.ReminderDataItem
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
+@Config(sdk = [Build.VERSION_CODES.P])
 class SaveReminderViewModelTest {
 
-
     //TODO: provide testing to the SaveReminderView and its live data objects
+//ros subjectUnderTest_actionOrInput_resultState
+    //GIVEN
+    //WHEN
+    //THEN
 
+    // Needed to test code with LiveData. If we do not use this, we will get the
+    // RuntimeException related to Looper in Android.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+
+    // Subject under test
+    private lateinit var saveReminderViewModel: SaveReminderViewModel
+    private lateinit var fakeDataSource: FakeDataSource
+    private val reminder = ReminderDataItem("Reminder 1", "Hi, remind that",
+        "American Airlines Arena", 25.781339, -80.187948,
+        "1000"
+    )
+
+    /**
+     * Before annotation (to create a setup method and remove repeated code) is used when you
+     * have repeated setup code like this vm shared between.
+     */
+    @Before
+    fun setupViewModel() {
+        fakeDataSource = FakeDataSource()
+
+        saveReminderViewModel = SaveReminderViewModel(ApplicationProvider.getApplicationContext(),
+            fakeDataSource
+        )
+    }
+
+   /* @Test
+    fun validateEnteredData_data_returnCorrect() {
+        // Given a fresh ViewModel
+        val saveReminderViewModel = SaveReminderViewModel(ApplicationProvider.getApplicationContext())
+        // And a reminder object
+        val reminder = ReminderDTO("Reminder 1", "Hi, remind that",
+            "American Airlines Arena", 25.781339, -80.187948,
+            "1000"
+        )
+
+        // When validating the data
+        val result = validateEnteredData(reminder)
+
+        // Then return true
+    }*/
+
+    @Test
+    fun givenData_whenValidateAndSaveReminder_returnSuccess() = runBlockingTest {
+
+        saveReminderViewModel.validateAndSaveReminder(reminder)
+        val result =
+            fakeDataSource.getReminder(reminder.id) as Result.Success<ReminderDTO>
+
+        val data = result.data
+        assertThat(data.id, equals("1000"))
+
+    }
 
 }
